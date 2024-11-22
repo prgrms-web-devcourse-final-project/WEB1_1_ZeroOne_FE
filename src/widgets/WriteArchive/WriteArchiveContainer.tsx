8213302cdd865124fde8f43102f52c4848c65ca6
he2e2 <dddd6368@naver.com>
2024-11-22 15:45:35 +0900

    feat: WriteArchivePage 추가

--- src/widgets/WriteArchive/WriteArchiveContainer.tsx
+++ src/widgets/WriteArchive/WriteArchiveContainer.tsx
@@ -4,7 +4,7 @@ export const WriteArchiveContainer = () => {
   return (
     <section className={styles.container}>
       <div className={styles.header}>
-        <h1>아카이브 작성</h1>
+        <h2>아카이브 작성</h2>
         <p>어떤 색상의 아카이브를 작성할지 골라주세요</p>
       </div>
       <div className={styles.contents}></div>
