b9ee1d5d094339327aa938dfae61a68b14791a8c
he2e2 <dddd6368@naver.com>
2024-11-22 12:47:36 +0900

    feat: appRouter에 Layout 추가

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import App from './App';
 import { worker } from '../mocks/browser';
 
 import './styles/globals.scss';
-import App from './App';
 
 if (process.env.NODE_ENV === 'development') {
   worker.start();
