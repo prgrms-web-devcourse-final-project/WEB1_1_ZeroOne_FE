6d2265724bafe4d3633a07ca7d7b6e83ab1af8cc
joarthvr <ioi456859@gmail.com>
2024-11-22 12:10:45 +0900

    etc: lighthouse ci setting

---
+++ lighthouserc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: 'npm run preview',
+      numberOfRuns: 3,
+      url: ['http://localhost:4173'],
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'categories:performance': ['warn', { minScore: 0.9 }],
+        'categories:accessibility': ['warn', { minScore: 0.9 }],
+        'categories:best-practices': ['warn', { minScore: 0.9 }],
+        'categories:seo': ['warn', { minScore: 0.9 }],
+      },
+    },
+  },
+};
