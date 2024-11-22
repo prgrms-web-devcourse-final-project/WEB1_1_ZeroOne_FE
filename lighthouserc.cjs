86501f90081859a82e2acc6de27c9d45c3c5a3f5
joarthvr <ioi456859@gmail.com>
2024-11-22 12:33:01 +0900

    etc: lighthouse 조건식 변경

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,9 +1,12 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: 'npm run preview',
+      startServerCommand: 'yarn dev',
       numberOfRuns: 3,
       url: ['http://localhost:4173'],
+      // 서버 시작 대기 설정 추가 (선택사항) 60초 기다림
+      startServerReadyPattern: 'ready started server on',
+      startServerReadyTimeout: 60000,
     },
     upload: {
       target: 'temporary-public-storage',
