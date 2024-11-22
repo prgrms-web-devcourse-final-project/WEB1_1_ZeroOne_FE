acd840f62ccba4c5fa273ac1f2a376d9a3d0d365
he2e2 <dddd6368@naver.com>
2024-11-22 12:41:40 +0900

    feat: base layout 추가

---
+++ src/widgets/Layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+
+export const Layout = () => {
+  return (
+    <>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default Layout;
