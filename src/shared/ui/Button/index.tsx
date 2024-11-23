74266ceef1953ccbed88a55dd9107146cb4d08e9
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-11-23 15:24:37 +0900

    etc: feat/#20-layout branch merge

---
+++ src/shared/ui/Button/index.tsx
@@ -0,0 +1,16 @@
+import type { ButtonHTMLAttributes } from 'react';
+import type React from 'react';
+
+import styles from './index.module.scss';
+
+/*
+ ** 필요한 버튼 Prop은 추가적으로 작성
+ ** ex) size, radius, fontsize ... 등
+ */
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export const Button = ({ children }: Props) => {
+  return <button className={styles.btn}>{children}</button>;
+};
--- src/widgets/Layout/index.tsx
+++ src/widgets/Layout/index.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from 'react-router-dom';
 
+import { Footer } from './ui/Footer';
+import { Header } from './ui/Header';
+
 export const Layout = () => {
   return (
     <>
+      <Header />
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 };
