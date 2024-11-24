cbb58e5d985ae2ca577cc997e8374c14d06268d8
채승규 <37896060+csk6314@users.noreply.github.com>
2024-11-24 19:14:14 +0900

    [FEAT] Layout ( Header, Footer) 컴포넌트 제작 (#22)
    * feat: shared ui Button 컴포넌트 제작
    
    * rename: Button.tsx -> index.tsx 이름 변경 및 내보내기 index파일 생성
    
    * fix: styles import 경로 변경
    
    * rename: Layout 폴더로 관리
    
    * chore: Header Assets 추가
    
    * chore: vite-svgr-plugin 설치
    
    * chore: vite-plugin-svgr 세팅
    
    * style: * box-sizing: border-box 옵션 추가
    
    * feat: nav-link constant
    
    * feat: Header 컴포넌트 구현
    
    * fix: import 경로 수정
    
    * style: header height variable 추가 & global style 수정
    
    * style: header-horizontal-padding variable 추가
    
    * feat: Footer 구현
    
    * remove: 불필요한 파일 삭제
    
    * fix: nav key prop 추가
    
    * design: header, footer semantic tag 적용
    
    * chore: identity-obj-proxy 패키지 설치
    
    * test: SVG Mock config + globals error fix
    
    * rename: index.tsx -> component.tsx 형식으로 변경

--- src/widgets/Layout/index.tsx
+++ src/widgets/Layout/index.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from 'react-router-dom';
 
+import { Footer } from './ui/Footer/Footer';
+import { Header } from './ui/Header/Header';
+
 export const Layout = () => {
   return (
     <>
+      <Header />
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 };
---
+++ src/widgets/Layout/ui/Header/Header.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+import styles from './Header.module.scss';
+import { NAV_LINKS } from '../../constants';
+
+//assets
+import Logo from '@/shared/assets/paletteLogo.svg?react';
+import SearchIcon from '@/shared/assets/search.svg?react';
+//component
+import { Button } from '@/shared/ui';
+
+export const Header = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <header className={styles.header}>
+      {/** Logo */}
+      <h1 className={styles.logo}>
+        <Link to='/'>
+          <Logo height={60} width={52} />
+        </Link>
+        <span>Palette</span>
+      </h1>
+      {/** Gnb */}
+      <nav className={styles.gnbWrapper}>
+        <ul className={styles.gnb}>
+          {NAV_LINKS.map((link, idx) => (
+            <React.Fragment key={link.path}>
+              <li>
+                <Link className={pathname === link.path ? styles.active : ''} to={link.path}>
+                  {link.title}
+                </Link>
+              </li>
+              {idx < NAV_LINKS.length - 1 && <li className={styles.navDot} />}
+            </React.Fragment>
+          ))}
+        </ul>
+      </nav>
+      {/** UserMenu */}
+      <div className={styles.userMenu}>
+        <SearchIcon height={31} width={31} />
+        <Button>로그인</Button>
+        <Button>회원가입</Button>
+      </div>
+    </header>
+  );
+};
