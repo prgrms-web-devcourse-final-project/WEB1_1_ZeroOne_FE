28b407150a67cf38f5ac9f15ec6b5553c5ab30f8
he2e2 <dddd6368@naver.com>
2024-11-24 21:56:51 +0900

    feat: 에디터 toolbar 기능 구현

--- src/features/Archive/ui/MarkdownEditor.tsx
+++ src/features/Archive/ui/MarkdownEditor.tsx
@@ -1,63 +1,187 @@
 import { markdown } from '@codemirror/lang-markdown';
+import { EditorSelection } from '@codemirror/state';
 import { EditorView } from '@codemirror/view';
 import { faCommentDots, faImage, faLink, faCode } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import CodeMirror from '@uiw/react-codemirror';
 import hljs from 'highlight.js';
+import type { DebouncedFunc } from 'lodash-es/debounce';
+import debounce from 'lodash-es/debounce';
 import { marked } from 'marked';
-import { useState, useCallback } from 'react';
+import { markedHighlight } from 'marked-highlight';
+import { useState, useRef } from 'react';
 import 'highlight.js/styles/github.css';
 
 import styles from './MarkdownEditor.module.scss';
 
-const renderer = new marked.Renderer();
+marked.use(
+  markedHighlight({
+    langPrefix: 'hljs language-',
+    highlight(code, lang) {
+      if (code.trim() === '') {
+        return hljs.highlight('', { language: 'plaintext' }).value;
+      }
 
-renderer.code = ({ text, lang }) => {
-  if (lang && hljs.getLanguage(lang)) {
-    try {
-      return hljs.highlight(text, { language: lang }).value;
-    } catch (error) {
-      if (error instanceof Error) console.error(error);
-    }
-  }
-  return hljs.highlightAuto(text).value;
-};
+      if (lang && hljs.getLanguage(lang)) {
+        return hljs.highlight(code, { language: lang }).value;
+      }
+      return hljs.highlight(code, { language: 'plaintext' }).value;
+    },
+  }),
+);
 
 marked.setOptions({
-  renderer,
   breaks: true,
+  gfm: true,
 });
 
 export const MarkdownEditor = () => {
+  const editorViewRef = useRef<EditorView | null>(null);
   const [markdownText, setMarkdownText] = useState<string>('');
 
-  const handleEditorChange = useCallback((value: string) => {
-    setMarkdownText(value);
-  }, []);
+  const getEditorContent = () => {
+    return editorViewRef.current?.state.doc.toString() || '';
+  };
+
+  const syncPreview: DebouncedFunc<() => void> = debounce(() => {
+    setMarkdownText(getEditorContent());
+  }, 10);
+
+  const insertStartToggle = (symbol: string, wrap: boolean = false) => {
+    const editorView = editorViewRef.current;
+    if (!editorView) return;
+
+    editorView.focus();
+
+    const { from, to } = editorView.state.selection.main;
+    const selectedText = editorView.state.sliceDoc(from, to);
+
+    if (wrap) {
+      const hasExist = selectedText.startsWith(symbol) && selectedText.endsWith(symbol);
+
+      if (!hasExist) {
+        editorView.dispatch({
+          changes: {
+            from,
+            to,
+            insert: `${symbol}${selectedText}${symbol}`,
+          },
+          selection: EditorSelection.range(from + symbol.length, to + symbol.length),
+        });
+      } else {
+        editorView.dispatch({
+          changes: {
+            from,
+            to,
+            insert: selectedText.slice(symbol.length, selectedText.length - symbol.length),
+          },
+          selection: EditorSelection.range(from, to - 2 * symbol.length),
+        });
+      }
+    } else if (symbol === '```') {
+      editorView.dispatch({
+        changes: {
+          from,
+          to,
+          insert: `\`\`\`\n${selectedText}\n\`\`\``,
+        },
+        selection: EditorSelection.cursor(to + 4),
+      });
+    } else {
+      const line = editorView.state.doc.lineAt(from);
+      const { from: lineStart, to: lineEnd, text } = line;
+
+      const hasExist = text.startsWith(symbol);
+
+      if (!hasExist) {
+        editorView.dispatch({
+          changes: {
+            from: lineStart,
+            to: lineEnd,
+            insert: `${symbol}${text}`,
+          },
+          selection: EditorSelection.cursor(lineStart + symbol.length),
+        });
+      } else {
+        editorView.dispatch({
+          changes: {
+            from: lineStart,
+            to: lineEnd,
+            insert: text.slice(symbol.length),
+          },
+          selection: EditorSelection.cursor(lineStart),
+        });
+      }
+    }
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.editor}>
         <div className={styles.toolbar}>
-          <button onClick={() => {}}>H1</button>
-          <button onClick={() => {}}>H2</button>
-          <button onClick={() => {}}>H3</button>
-          <button className={styles.bold} onClick={() => {}}>
+          <button
+            onClick={() => {
+              insertStartToggle('# ');
+            }}
+          >
+            H1
+          </button>
+          <button
+            onClick={() => {
+              insertStartToggle('## ');
+            }}
+          >
+            H2
+          </button>
+          <button
+            onClick={() => {
+              insertStartToggle('### ');
+            }}
+          >
+            H3
+          </button>
+          <button
+            className={styles.bold}
+            onClick={() => {
+              insertStartToggle('**', true);
+            }}
+          >
             B
           </button>
-          <button className={styles.italic} onClick={() => {}}>
+          <button
+            className={styles.italic}
+            onClick={() => {
+              insertStartToggle('_', true);
+            }}
+          >
             I
           </button>
-          <button onClick={() => {}}>
+          <button
+            onClick={() => {
+              insertStartToggle('> ');
+            }}
+          >
             <FontAwesomeIcon icon={faCommentDots} />
           </button>
-          <button onClick={() => {}}>
+          <button
+            onClick={() => {
+              insertStartToggle('```');
+            }}
+          >
             <FontAwesomeIcon icon={faCode} />
           </button>
-          <button onClick={() => {}}>
+          <button
+            onClick={() => {
+              insertStartToggle('[Link](url)');
+            }}
+          >
             <FontAwesomeIcon icon={faLink} />
           </button>
-          <button onClick={() => {}}>
+          <button
+            onClick={() => {
+              insertStartToggle('![Image](url)');
+            }}
+          >
             <FontAwesomeIcon icon={faImage} />
           </button>
         </div>
@@ -73,19 +197,18 @@ export const MarkdownEditor = () => {
               '.cm-activeLine': { backgroundColor: 'transparent' },
             }),
           ]}
-          onChange={handleEditorChange}
-          value={markdownText}
+          onUpdate={update => {
+            if (update.view) {
+              editorViewRef.current = update.view;
+              syncPreview();
+            }
+          }}
         />
       </div>
       <div
         className={styles.mirror}
         dangerouslySetInnerHTML={{
-          __html: marked(
-            markdownText
-              .split(/\n{2,}/)
-              .map(block => `<p>${block.replace(/\n/g, '<br />')}</p>`)
-              .join(''),
-          ),
+          __html: marked(markdownText),
         }}
       />
     </div>
--- src/features/Archive/ui/index.ts
+++ src/features/Archive/ui/index.ts
@@ -1,1 +1,2 @@
 export * from './ColorChip';
+export * from './MarkdownEditor';
