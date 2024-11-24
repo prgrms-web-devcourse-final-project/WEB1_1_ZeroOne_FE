f7ff82c24d9b816c3cf31c0a45243ad1e7601dde
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-11-25 04:28:11 +0900

    fix: Modal 컴포넌트 mount 상태 값 추가

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import cs from 'classnames';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 import styles from './Modal.module.scss';
@@ -13,12 +13,14 @@ interface Props {
 
 export const Modal = ({ isOpen, onClose, children, classNames }: Props) => {
   const rootModalDiv = useRef<HTMLElement | null>(null);
+  const [mount, setMount] = useState<boolean>(false);
 
   useEffect(() => {
     rootModalDiv.current = document.getElementById('modal');
+    setMount(true);
   }, []);
 
-  if (!rootModalDiv.current || !isOpen) return null;
+  if (!rootModalDiv.current || !mount || !isOpen) return null;
 
   return createPortal(
     <div className={styles.modalBackdrop}>
