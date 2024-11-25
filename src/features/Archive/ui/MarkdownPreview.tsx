e197f8ae5cbd6d9d0bd211c0591a1b71790cfcae
he2e2 <dddd6368@naver.com>
2024-11-25 11:42:33 +0900

    feat: 코드 하이라이팅 적용

--- src/features/Archive/ui/MarkdownPreview.tsx
+++ src/features/Archive/ui/MarkdownPreview.tsx
@@ -1,7 +1,30 @@
+import hljs from 'highlight.js';
 import { marked } from 'marked';
+import { markedHighlight } from 'marked-highlight';
 
 import 'highlight.js/styles/github.css';
 import styles from './MarkdownPreview.module.scss';
+
+marked.use(
+  markedHighlight({
+    langPrefix: 'hljs language-',
+    highlight(code, lang) {
+      if (code.trim() === '') {
+        return hljs.highlight('', { language: 'plaintext' }).value;
+      }
+
+      if (lang && hljs.getLanguage(lang)) {
+        return hljs.highlight(code, { language: lang }).value;
+      }
+      return hljs.highlight(code, { language: 'plaintext' }).value;
+    },
+  }),
+);
+
+marked.setOptions({
+  breaks: true,
+  gfm: true,
+});
 
 type MarkdownPreviewProps = {
   markdownText: string;
