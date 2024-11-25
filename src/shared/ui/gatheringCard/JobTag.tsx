c3b086f71e25bf1740dfce5517207bbb68c09406
joarthvr <ioi456859@gmail.com>
2024-11-25 10:38:46 +0900

    fix: reset git cache for case sensitivity

--- src/shared/ui/gatheringCard/JobTag.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './JobTag.module.scss';
-
-interface JobTagProps {
-  job: string;
-}
-export const JobTag = ({ job }: JobTagProps) => {
-  return <div className={styles.jobTag}>{job}</div>;
-};
