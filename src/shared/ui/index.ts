9a7dc51261151d8fb0bf902d8c5aed96c4bbcf8a
채승규 <37896060+csk6314@users.noreply.github.com>
2024-11-26 12:42:36 +0900

    [Feat] 폼 컴포넌트 구현 (#41)
    * feat: Radio 컴포넌트 구현
    
    * feat: Input 컴포넌트 구현
    
    * feat: index에 Radio, Input 추가
    
    * design: active color 추가, input font inherit 속성 추가

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,9 +2,9 @@ export { Button } from './Button/Button';
 export { Modal } from './Modal/Modal';
 export { GatheringCard } from './GatheringCard/GatheringCard';
 export { JobTag } from './GatheringCard/JobTag';
-
 export { SelectBtn } from './SelectBtn/SelectBtn';
-
 export { MarkdownEditor } from './MarkdownEditor/MarkdownEditor';
+export { Input } from './Input/Input';
+export { Radio } from './Radio/Radio';
 export { Switch } from './Switch/Switch';
 export { Tag } from './Tag/Tag';
