c5da8adde9ef1225117cda19e192a75b595edd21
joarthvr <ioi456859@gmail.com>
2024-11-26 09:08:24 +0900

    feat: 게더링 페이지 레이아웃 정리

--- src/pages/GatheringListPage/GatheringListPage.tsx
+++ src/pages/GatheringListPage/GatheringListPage.tsx
@@ -1,13 +1,18 @@
-import cn from 'classnames';
+// import cn from 'classnames';
 
 import styles from './GatheringListPage.module.scss';
 
-import { GatheringGrid } from '@/widgets';
-
+// import { GatheringGrid } from '@/widgets';
+import { GatheringSelectCon, GatheringGrid } from '@/widgets';
 export const GatheringListPage = () => {
   return (
-    <div className={cn(styles.page)}>
-      <GatheringGrid />
+    <div>
+      {/* 사이드 메뉴 */}
+      <div>
+        <h1 className={styles.h1}>프로젝트(64)</h1>
+        <GatheringSelectCon />
+        <GatheringGrid />
+      </div>
     </div>
   );
 };
