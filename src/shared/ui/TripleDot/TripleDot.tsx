241251a1cc680e787101325930ce8223b6b2055e
joarthvr <ioi456859@gmail.com>
2024-11-27 02:19:24 +0900

    feat: 세 점 컴포넌트 구현

--- src/shared/ui/TripleDot/TripleDot.tsx
+++ src/shared/ui/TripleDot/TripleDot.tsx
@@ -1,11 +1,18 @@
+import cn from 'classnames';
+
 import styles from './TripleDot.module.scss';
 
-export const TripleDot = () => {
+interface TripleDotProps {
+  className?: string; // 외부 컨테이너 스타일링 props
+  dotStyle?: string; // 외부 도트 스타일링 props
+}
+
+export const TripleDot = ({ className, dotStyle }: TripleDotProps) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.dot}></div>
-      <div className={styles.dot}></div>
-      <div className={styles.dot}></div>
+    <div className={cn(styles.container, className)}>
+      {Array.from({ length: 3 }).map((_, index) => (
+        <div className={cn(styles.dot, dotStyle)} key={index}></div>
+      ))}
     </div>
   );
 };
