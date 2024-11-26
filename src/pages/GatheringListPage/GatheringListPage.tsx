6dce9ba7a1581e6dab9976d82741070c07f7d74c
joarthvr <ioi456859@gmail.com>
2024-11-27 02:25:47 +0900

    feat: 게더링 리스트 페이지 레이아웃 구성

--- src/pages/GatheringListPage/GatheringListPage.tsx
+++ src/pages/GatheringListPage/GatheringListPage.tsx
@@ -1,17 +1,24 @@
-// import cn from 'classnames';
-
+// GatheringListPage.tsx
 import styles from './GatheringListPage.module.scss';
+import { PROJECT_CATEGORIES } from '../../shared/ui/SidebarFilter/config/constants';
 
-// import { GatheringGrid } from '@/widgets';
+import { SidebarFilter } from '@/shared/ui';
 import { GatheringSelectCon, GatheringGrid } from '@/widgets';
+
 export const GatheringListPage = () => {
   return (
-    <div>
-      {/* 사이드 메뉴 */}
-      <div>
-        <h1 className={styles.h1}>프로젝트(64)</h1>
-        <GatheringSelectCon />
-        <GatheringGrid />
+    <div className={styles.pageWrapper}>
+      <h1 className={styles.h1}>프로젝트(64)</h1>
+      <div className={styles.contentContainer}>
+        <div className={styles.sidebarWrapper}>
+          <aside className={styles.sidebarContainer}>
+            <SidebarFilter categories={PROJECT_CATEGORIES} />
+          </aside>
+        </div>
+        <div className={styles.mainContent}>
+          <GatheringSelectCon />
+          <GatheringGrid />
+        </div>
       </div>
     </div>
   );
