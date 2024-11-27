fd673066bd55a61d65fc2994a26599a1856b51a5
joarthvr <ioi456859@gmail.com>
2024-11-27 10:17:20 +0900

    feat: index 정리

--- src/pages/GatheringListPage/GatheringListPage.tsx
+++ src/pages/GatheringListPage/GatheringListPage.tsx
@@ -1,20 +1,17 @@
 // GatheringListPage.tsx
 import styles from './GatheringListPage.module.scss';
-import { PROJECT_CATEGORIES } from '../../shared/ui/SidebarFilter/config/constants';
 
-import { JOB_CATEGORIES } from '@/shared/model/jobCategories';
-import { SidebarFilter } from '@/shared/ui';
+import { SidebarFilter, PROJECT_CATEGORIES } from '@/shared/ui';
 import { GatheringSelectCon, GatheringGrid } from '@/widgets';
 
 export const GatheringListPage = () => {
   return (
     <div className={styles.pageWrapper}>
-      <h1 className={styles.h1}>프로젝트(64)</h1>
+      <h1 className={styles.h1}>프로젝트</h1>
       <div className={styles.contentContainer}>
         <div className={styles.sidebarWrapper}>
           <aside className={styles.sidebarContainer}>
-            {/* <SidebarFilter categories={PROJECT_CATEGORIES} /> */}
-            <SidebarFilter categories={JOB_CATEGORIES} />
+            <SidebarFilter categories={PROJECT_CATEGORIES} />
           </aside>
         </div>
         <div className={styles.mainContent}>
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -8,6 +8,5 @@ export { SelectBtn } from './SelectBtn/SelectBtn';
 export { MarkdownEditor } from './MarkdownEditor/MarkdownEditor';
 export { Switch } from './Switch/Switch';
 export { Tag } from './Tag/Tag';
-export { SidebarFilter } from './SidebarFilter/ui/SidebarFilter';
 export { TripleDot } from './TripleDot/TripleDot';
-export * from './SidebarFilter';
+export * from './SidebarFilter/index';
