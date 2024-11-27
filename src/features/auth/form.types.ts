2b4cf1ea022dfe402b4c5dd0d56f9733a21ba97c
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-11-28 00:00:00 +0900

    feat: 직군, 직무 데이터 변환 & formConfig utils 이동

--- src/features/auth/form.types.ts
+++ src/features/auth/form.types.ts
@@ -1,3 +1,5 @@
+import type { ObjectSchema } from 'yup';
+
 export type Option = {
   value: string;
   label: string;
@@ -37,146 +39,92 @@ interface FieldSetInfo {
   inputs: InputInfo[];
 }
 
-interface FormConfig {
+export interface FormConfigType {
   structure: FieldSetInfo[];
+  validation: ObjectSchema<FormValues>;
 }
 
-export const JOB_CATEGORIES = {
-  '개발(DEVELOPER)': [
-    '프론트엔드 개발자(FRONTEND)',
-    '서버/백엔드 개발자(BACKEND)',
-    '풀스택 개발자(FULL_STACK)',
-    '소프트웨어 엔지니어(SOFTWARE)',
-    'Android 개발자(ANDROID)',
-    'IOS 개발자(IOS)',
-    '크로스플랫폼 앱 개발자(CROSS_PLATFORM)',
-    'DevOps 엔지니어(DEV_OPS)',
-    '시스템/네트워크 관리(SYSTEM)',
-    '머신러닝 엔지니어(MACHINE_LEARNING)',
-    'QA 엔지니어(QA)',
-    '데이터 엔지니어(DATA_ENGINEER)',
-    '데이터 사이언티스트(DATA_SCIENCE)',
-    '보안 엔지니어(SECURITY)',
-    'HW/임베디드 개발자(HW_EMBEDDED)',
-    '블록체인 엔지니어(BLOCK_CHAIN)',
-    'DBA(DBA)',
-    '게임 개발자(GAME)',
-    '기타(ETC_DEV)',
-  ],
-  '기획(PROMOTER)': [
-    'PM ∘ PO(PM_PO)',
-    '서비스 기획자(SERVICE)',
-    '전략 기획자(STRATEGY)',
-    '사업개발 기획자(BUSINESS)',
-    '비즈니스 분석가(ANALYSIS)',
-    '상품 기획자/MD(MD)',
-    '다른 기획 분야(ETC_PROM)',
-  ],
-  '디자인(DESIGN)': [
-    '일반 디자인(DESIGN)',
-    '인스퍼레이션(INSPIRATION)',
-    'UI/UX(UI_UX)',
-    '코딩하는 디자이너(WITH_CODING)',
-    '브랜드 디자이너(BRAND)',
-    '타이포그래피(TYPO)',
-    '디자인 리소스(RESOURCE)',
-  ],
-  '마케팅(MARKETING)': [
-    '일반 마케팅(MARKETING)',
-    '브랜드 마케팅(BRAND)',
-    '그로스 마케팅(GROWTH)',
-    '콘텐츠 마케팅(CONTENTS)',
-    '마케팅 인사이트(INSIGHT)',
-  ],
-  '기타(ETC)': [
-    '서비스 업(SERVICE)',
-    '판매, 유통(DISTRIBUTION)',
-    '건설업(CONSTRUCT)',
-    '운전, 운송, 배송(DELIVERY)',
-    '교육업(EDUCATION)',
-    '은행, 금융업(FINANCE)',
-    '공공, 복지(WELFARE)',
-    '그 외(ETC)',
-  ],
-} as const;
+export const JOB_CATEGORIES = [
+  {
+    label: '개발',
+    value: 'DEVELOPER',
+    children: [
+      { label: '프론트엔드 개발자', value: 'FRONTEND' },
+      { label: '서버/백엔드 개발자', value: 'BACKEND' },
+      { label: '풀스택 개발자', value: 'FULL_STACK' },
+      { label: '소프트웨어 엔지니어', value: 'SOFTWARE' },
+      { label: 'Android 개발자', value: 'ANDROID' },
+      { label: 'IOS 개발자', value: 'IOS' },
+      { label: '크로스플랫폼 앱 개발자', value: 'CROSS_PLATFORM' },
+      { label: 'DevOps 엔지니어', value: 'DEV_OPS' },
+      { label: '시스템/네트워크 관리', value: 'SYSTEM' },
+      { label: '머신러닝 엔지니어', value: 'MACHINE_LEARNING' },
+      { label: 'QA 엔지니어', value: 'QA' },
+      { label: '데이터 엔지니어', value: 'DATA_ENGINEER' },
+      { label: '데이터 사이언티스트', value: 'DATA_SCIENCE' },
+      { label: '보안 엔지니어', value: 'SECURITY' },
+      { label: 'HW/임베디드 개발자', value: 'HW_EMBEDDED' },
+      { label: '블록체인 엔지니어', value: 'BLOCK_CHAIN' },
+      { label: 'DBA', value: 'DBA' },
+      { label: '게임 개발자', value: 'GAME' },
+      { label: '기타', value: 'ETC_DEV' },
+    ],
+  },
+  {
+    label: '기획',
+    value: 'PROMOTER',
+    children: [
+      { label: 'PM ∘ PO', value: 'PM_PO' },
+      { label: '서비스 기획자', value: 'SERVICE' },
+      { label: '전략 기획자', value: 'STRATEGY' },
+      { label: '사업개발 기획자', value: 'BUSINESS' },
+      { label: '비즈니스 분석가', value: 'ANALYSIS' },
+      { label: '상품 기획자/MD', value: 'MD' },
+      { label: '다른 기획 분야', value: 'ETC_PROM' },
+    ],
+  },
+  {
+    label: '디자인',
+    value: 'DESIGN',
+    children: [
+      { label: '일반 디자인', value: 'DESIGN' },
+      { label: '인스퍼레이션', value: 'INSPIRATION' },
+      { label: 'UI/UX', value: 'UI_UX' },
+      { label: '코딩하는 디자이너', value: 'WITH_CODING' },
+      { label: '브랜드 디자이너', value: 'BRAND' },
+      { label: '타이포그래피', value: 'TYPO' },
+      { label: '디자인 리소스', value: 'RESOURCE' },
+    ],
+  },
+  {
+    label: '마케팅',
+    value: 'MARKETING',
+    children: [
+      { label: '일반 마케팅', value: 'MARKETING' },
+      { label: '브랜드 마케팅', value: 'BRAND' },
+      { label: '그로스 마케팅', value: 'GROWTH' },
+      { label: '콘텐츠 마케팅', value: 'CONTENTS' },
+      { label: '마케팅 인사이트', value: 'INSIGHT' },
+    ],
+  },
+  {
+    label: '기타',
+    value: 'ETC',
+    children: [
+      { label: '서비스 업', value: 'SERVICE' },
+      { label: '판매, 유통', value: 'DISTRIBUTION' },
+      { label: '건설업', value: 'CONSTRUCT' },
+      { label: '운전, 운송, 배송', value: 'DELIVERY' },
+      { label: '교육업', value: 'EDUCATION' },
+      { label: '은행, 금융업', value: 'FINANCE' },
+      { label: '공공, 복지', value: 'WELFARE' },
+      { label: '그 외', value: 'ETC' },
+    ],
+  },
+];
 
-export type MajorJobType = keyof typeof JOB_CATEGORIES;
-
-const JOB_DIVISION: Option[] = [
+export const JOB_DIVISION: Option[] = [
   { value: 'student', label: '학생' },
   { value: 'worker', label: '회사' },
   { value: 'etc', label: '기타' },
 ];
-
-export const FORM_CONFIG: FormConfig = {
-  structure: [
-    {
-      title: '기본 정보',
-      inputs: [
-        {
-          label: '프로필 사진',
-          type: 'image',
-          name: 'imageUrl',
-        },
-        {
-          label: '이름',
-          type: 'default',
-          name: 'name',
-          required: true,
-          placeholder: '이름을 입력해주세요.',
-        },
-        {
-          label: '한 줄 소개',
-          type: 'textarea',
-          name: 'briefIntro',
-          maxLength: 100,
-          placeholder: '한 줄 소개를 입력해주세요.',
-        },
-      ],
-    },
-    {
-      title: '커리어 정보',
-      inputs: [
-        {
-          label: '분야',
-          type: 'select',
-          name: 'majorJobGroup',
-          options: Object.keys(JOB_CATEGORIES).map(category => ({
-            value: category,
-            label: category,
-          })),
-          placeholder: '분야를 골라주세요.',
-        },
-        {
-          label: '직무',
-          type: 'select',
-          name: 'minorJobGroup',
-          placeholder: '분야를 먼저 선택해 주세요.',
-        },
-        {
-          label: '소속',
-          type: 'radio',
-          name: 'division',
-          options: JOB_DIVISION,
-        },
-        {
-          label: '소속명',
-          type: 'default',
-          name: 'jobTitle',
-          placeholder: '소속명을 입력해주세요.',
-        },
-      ],
-    },
-    {
-      title: 'URL',
-      inputs: [
-        {
-          label: 'URL',
-          type: 'array',
-          name: 'url',
-          placeholder: 'https://',
-        },
-      ],
-    },
-  ],
-};
