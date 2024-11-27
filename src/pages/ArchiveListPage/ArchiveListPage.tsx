4d285ed7e7e48667f8128190281e3e29c6360692
he2e2 <dddd6368@naver.com>
2024-11-28 00:42:30 +0900

    feat: selectButton

--- src/pages/ArchiveListPage/ArchiveListPage.tsx
+++ src/pages/ArchiveListPage/ArchiveListPage.tsx
@@ -1,10 +1,23 @@
 import styles from './ArchiveListPage.module.scss';
 
+import { SelectBtn } from '@/shared/ui';
 import { ArchiveGrid } from '@/widgets';
 
 export const ArchiveListPage = () => {
   return (
     <div className={styles.wrapper}>
+      <div className={styles.selectWrapper}>
+        <h2>아카이브 리스트 색상</h2>
+        <div className={styles.orderWrapper}>
+          <SelectBtn
+            options={[
+              { label: '최신순', value: 'latest' },
+              { label: '인기순', value: 'popular' },
+            ]}
+            value={{ label: '최신순', value: 'latest' }}
+          />
+        </div>
+      </div>
       <ArchiveGrid />
     </div>
   );
