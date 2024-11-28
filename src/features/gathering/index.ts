6c386b0f4556d2f6c130249510772b2040c6bb20
joarthvr <ioi456859@gmail.com>
2024-11-28 11:06:26 +0900

    feat: 게더링 페이지 레이아웃 구현

--- src/features/gathering/index.ts
+++ src/features/gathering/index.ts
@@ -1,2 +1,8 @@
 export * from './model/options';
 export * from './model/types';
+export { GatheringSelect } from './ui/GatheringSelect';
+export { GatheringDatePicker } from './ui/GatheringDatePicker';
+export { GatheringTagInput } from './ui/GatheringTagInput';
+export { GatheringLinkInput } from './ui/GatheringLinkInput';
+export { GatheringTitleInput } from './ui/GatheringTitIeInput';
+export { GatheringMarkdownEditor } from './ui/GatheringMarkdownEditor';
--- src/pages/WriteGatheringPage/WriteGatheringPage.tsx
+++ src/pages/WriteGatheringPage/WriteGatheringPage.tsx
@@ -1,10 +1,50 @@
+// WriteGatheringPage.tsx
+import { FormProvider, useForm } from 'react-hook-form';
+
 import styles from './WriteGatheringPage.module.scss';
 
-import { customConfirm } from '@/shared/ui';
+import type { GatheringFormData } from '@/features/gathering/model/types';
+import { WriteGatheringOpts, WriteGatheringDetail } from '@/widgets';
+
 export const WriteGatheringPage = () => {
-  const result = customConfirm({
-    title: '내용 초기화',
-    text: '작성 중인 내용을 초기화하시겠습니까?',
+  const methods = useForm<GatheringFormData>({
+    defaultValues: {
+      contact: '',
+      sort: '',
+      subject: '',
+      period: '',
+      personnel: '',
+      position: [],
+      gatheringTag: [],
+      title: '',
+      url: '',
+      content: '', // 마크다운 에디터 내용
+      deadLine: '',
+    },
   });
-  return <div className={styles.container}>WriteGatheringPage</div>;
+
+  const onSubmit = (data: GatheringFormData) => {
+    console.log('제출된 데이터:', data);
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <div className={styles.container}>
+        <h1 className={styles.h1}>게더링 작성</h1>
+        <form className={styles.form} onSubmit={methods.handleSubmit(onSubmit)}>
+          <section className={styles.section}>
+            <h2 className={styles.h2}>1. 게더링 기본 정보를 등록해주세요.</h2>
+            <WriteGatheringOpts control={methods.control} />
+          </section>
+          <section className={styles.section}>
+            <h2 className={styles.h2}>2. 게시글에 대해서 더 자세히 설명해주세요</h2>
+            <WriteGatheringDetail control={methods.control} />
+          </section>
+          <button className={styles.submitButton} type='submit'>
+            등록하기
+          </button>
+        </form>
+      </div>
+    </FormProvider>
+  );
 };
