8421706b6e25d6fcfa3f5b9c5578e10550ef265c
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-11-29 03:27:12 +0900

    feat: layout main no padding 예외 추가

--- src/widgets/Layout/index.tsx
+++ src/widgets/Layout/index.tsx
@@ -1,13 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import cn from 'classnames';
+import { Outlet, useLocation } from 'react-router-dom';
 
+//constant
+import { NO_PAD_ROUTES } from './constants';
+//style
+import styles from './Layout.module.scss';
+//component
 import { Footer } from './ui/Footer/Footer';
 import { Header } from './ui/Header/Header';
 
 export const Layout = () => {
+  const { pathname } = useLocation();
+  const isNoPadHeader = NO_PAD_ROUTES.includes(pathname);
+
   return (
     <>
       <Header />
-      <main>
+      <main className={cn({ [styles.noPadding]: isNoPadHeader })}>
         <Outlet />
       </main>
       <Footer />
