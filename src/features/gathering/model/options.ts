bdc1b78ca0470ded2b85e66aae79f1f12df8333a
joarthvr <ioi456859@gmail.com>
2024-11-28 10:51:36 +0900

    feat: 게더링에서 사용될 옵션 타입 정의

--- src/features/gathering/model/options.ts
+++ src/features/gathering/model/options.ts
@@ -1,24 +1,24 @@
 import type { GatheringFilterOptions } from './types';
 
 export const gatheringFilterOptions: GatheringFilterOptions = {
-  processType: [
+  contact: [
     { value: '온라인', label: '온라인' },
     { value: '오프라인', label: '오프라인' },
-    { value: '온라인&오프라인', label: '온라인 & 오프라인' },
+    { value: '온라인&오프라인', label: '온라인&오프라인' },
   ],
-  category: [
+  sort: [
     { value: '스터디', label: '스터디' },
     { value: '프로젝트', label: '프로젝트' },
     { value: '동아리', label: '동아리' },
     { value: '기타', label: '기타' },
   ],
-  term: [
+  period: [
     { value: '1개월', label: '1개월' },
     { value: '3개월', label: '3개월' },
     { value: '6개월', label: '6개월' },
     { value: '6개월 이상', label: '6개월 이상' },
   ],
-  recruitment: [
+  personnel: [
     { value: '1', label: '1' },
     { value: '2', label: '2' },
     { value: '3', label: '3' },
@@ -36,4 +36,26 @@ export const gatheringFilterOptions: GatheringFilterOptions = {
     { value: '기획자', label: '기획자' },
     { value: '마케터', label: '마케터' },
   ],
-} as const;
+  subject: {
+    project: [
+      { value: '개발', label: '개발' },
+      { value: '디자인', label: '디자인' },
+      { value: '기획', label: '기획' },
+      { value: '마케팅', label: '마케팅' },
+      { value: '기타', label: '기타' },
+    ],
+    study: [
+      { value: '개발', label: '개발' },
+      { value: '디자인', label: '디자인' },
+      { value: '어학', label: '어학' },
+      { value: '기타', label: '기타' },
+    ],
+    club: [
+      { value: '취미', label: '취미' },
+      { value: '운동', label: '운동' },
+      { value: '음악', label: '음악' },
+      { value: '기타', label: '기타' },
+    ],
+    etc: [{ value: '기타', label: '기타' }],
+  },
+};
--- src/features/gathering/model/types.ts
+++ src/features/gathering/model/types.ts
@@ -1,28 +1,39 @@
-export type GatheringProcessType = '온라인' | '오프라인' | '온라인&오프라인';
-export type GatheringCategory = '스터디' | '프로젝트' | '동아리' | '기타';
-export type GatheringTerm = '1개월' | '3개월' | '6개월' | '6개월 이상';
-export type GatheringRecruitment =
-  | '1'
-  | '2'
-  | '3'
-  | '4'
-  | '5'
-  | '6'
-  | '7'
-  | '8'
-  | '9'
-  | '10명 이상';
+export type GatheringContactType = '온라인' | '오프라인' | '온라인&오프라인';
+export type GatheringSortType = '스터디' | '프로젝트' | '동아리' | '기타';
+export type GatheringSubjectType = '개발' | '디자인' | '기획' | '마케팅';
+export type GatheringPeriod = '1개월' | '3개월' | '6개월' | '6개월 이상';
+export type GatheringPersonnel = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10명 이상';
 export type GatheringPosition = '개발자' | '디자이너' | '기획자' | '마케터';
 
-export interface SelectOption<T> {
-  value: T;
+export type SelectOption = {
+  value: string;
   label: string;
+};
+
+export interface GatheringFormData {
+  contact: string;
+  sort: string;
+  subject: string;
+  period: string;
+  personnel: string;
+  position: string[];
+  gatheringTag: string[];
+  title: string;
+  url: string;
+  content: string;
+  deadLine: string ;
 }
 
 export interface GatheringFilterOptions {
-  processType: SelectOption<GatheringProcessType>[];
-  category: SelectOption<GatheringCategory>[];
-  term: SelectOption<GatheringTerm>[];
-  recruitment: SelectOption<GatheringRecruitment>[];
-  position: SelectOption<GatheringPosition>[];
+  contact: SelectOption[];
+  sort: SelectOption[];
+  period: SelectOption[];
+  personnel: SelectOption[];
+  position: SelectOption[];
+  subject: {
+    project: SelectOption[];
+    study: SelectOption[];
+    club: SelectOption[];
+    etc: SelectOption[];
+  };
 }
