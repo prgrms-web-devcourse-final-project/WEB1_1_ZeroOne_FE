ef433dca48d5159491f700151524dcfe4f515395
joarthvr <ioi456859@gmail.com>
2024-11-29 15:56:53 +0900

    feat: gathering detail page useParams 설정

--- src/pages/GatheringDetailPage/GatheringDetailPage.tsx
+++ src/pages/GatheringDetailPage/GatheringDetailPage.tsx
@@ -1,4 +1,7 @@
+import { useParams } from 'react-router-dom';
+
 import styles from './GatheringDetailPage.module.scss';
 export const GatheringDetailPage = () => {
-  return <div className={styles.container}>GatheringDetail</div>;
+  const { gatheringId } = useParams();
+  return <div className={styles.container}>GatheringDetail{gatheringId}</div>;
 };
