be2f07f80205011ef8dfc842298fa731b747251c
joarthvr <ioi456859@gmail.com>
2024-11-29 12:39:52 +0900

    feature: widget 포트폴리오 그리드 위젝 구현

--- src/widgets/PortfolioGrid/PortFolioGrid.tsx
+++ src/widgets/PortfolioGrid/PortFolioGrid.tsx
@@ -1,10 +1,80 @@
 import styles from './PortFolioGrid.module.scss';
 
 import { PortfolioCard } from '@/features';
+import type { Portfolio } from '@/features';
+
 export const PortFolioGrid = () => {
+  const portfolios: Portfolio[] = [
+    {
+      portFolioId: 28,
+      portFolioUrl: 'https://example.com/portfolio/28',
+      username: '리성원',
+      introduction: '안녕하세요',
+      majorJobGroup: '개발',
+      minorJobGroup: '서버/백엔드 개발자',
+      memberImageUrl: 'fdsf',
+    },
+    {
+      portFolioId: 28,
+      portFolioUrl: 'https://example.com/portfolio/28',
+      username: '리성원',
+      introduction: '안녕하세요',
+      majorJobGroup: '개발',
+      minorJobGroup: '서버/백엔드 개발자',
+      memberImageUrl: 'fdsf',
+    },
+    {
+      portFolioId: 28,
+      portFolioUrl: 'https://example.com/portfolio/28',
+      username: '리성원',
+      introduction: '안녕하세요',
+      majorJobGroup: '개발',
+      minorJobGroup: '서버/백엔드 개발자',
+      memberImageUrl: 'fdsf',
+    },
+    {
+      portFolioId: 28,
+      portFolioUrl: 'https://example.com/portfolio/28',
+      username: '리성원',
+      introduction: '안녕하세요',
+      majorJobGroup: '개발',
+      minorJobGroup: '서버/백엔드 개발자',
+      memberImageUrl: 'fdsf',
+    },
+    {
+      portFolioId: 28,
+      portFolioUrl: 'https://example.com/portfolio/28',
+      username: '리성원',
+      introduction: '안녕하세요',
+      majorJobGroup: '개발',
+      minorJobGroup: '서버/백엔드 개발자',
+      memberImageUrl: 'fdsf',
+    },
+    {
+      portFolioId: 28,
+      portFolioUrl: 'https://example.com/portfolio/28',
+      username: '리성원',
+      introduction: '안녕하세요',
+      majorJobGroup: '개발',
+      minorJobGroup: '서버/백엔드 개발자',
+      memberImageUrl: 'fdsf',
+    },
+    {
+      portFolioId: 28,
+      portFolioUrl: 'https://example.com/portfolio/28',
+      username: '리성원',
+      introduction: '안녕하세요',
+      majorJobGroup: '개발',
+      minorJobGroup: '서버/백엔드 개발자',
+      memberImageUrl: 'fdsf',
+    },
+  ];
+
   return (
     <div className={styles.container}>
-      <PortfolioCard />
+      {portfolios.map(portfolio => (
+        <PortfolioCard key={portfolio.portFolioId} {...portfolio} />
+      ))}
     </div>
   );
 };
