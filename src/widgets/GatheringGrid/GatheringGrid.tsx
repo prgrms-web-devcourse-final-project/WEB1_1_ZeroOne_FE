d047d4075c191d3aed1d6fbd99a9c81558582303
joarthvr <ioi456859@gmail.com>
2024-11-30 15:12:28 +0900

    feat: 게더링 연관된 페이지 props 전달

--- src/widgets/GatheringGrid/GatheringGrid.tsx
+++ src/widgets/GatheringGrid/GatheringGrid.tsx
@@ -1,155 +1,27 @@
 import styles from './GatheringGrid.module.scss';
 
-import { GatheringCard } from '@/shared/ui';
-export const GatheringGrid = () => {
+import type { GatheringItemDto } from '@/features/gathering/model/gathering.dto';
+import { GatheringCard } from '@/features/gathering/ui/GatheringCard';
+
+interface GatheringGridProps {
+  items: GatheringItemDto[];
+}
+
+export const GatheringGrid = ({ items }: GatheringGridProps) => {
   return (
     <article className={styles.container}>
       <ul className={styles.list}>
-        <GatheringCard
-          deadline='deadline'
-          introduction='introduction'
-          name='name'
-          tag={['tag1', 'tag2']}
-          title='title'
-        />
-        <GatheringCard
-          deadline='deadline'
-          introduction='introduction'
-          name='name'
-          tag={['tag1', 'tag2']}
-          title='title'
-        />
-        <GatheringCard
-          deadline='deadline'
-          introduction='introduction'
-          name='name'
-          tag={['tag1', 'tag2']}
-          title='title'
-        />
-        <GatheringCard
-          deadline='deadline'
-          introduction='introduction'
-          name='name'
-          tag={['tag1', 'tag2']}
-          title='title'
-        />
-        <GatheringCard
-          deadline='deadline'
-          introduction='introduction'
-          name='name'
-          tag={['tag1', 'tag2']}
-          title='title'
-        />
-        <GatheringCard
-          deadline='deadline'
-          introduction='introduction'
-          name='name'
-          tag={['tag1', 'tag2']}
-          title='title'
-        />
-        <GatheringCard
-          deadline='deadline'
-          introduction='introduction'
-          name='name'
-          tag={['tag1', 'tag2']}
-          title='title'
-        />
-        <GatheringCard
-          deadline='deadline'
-          introduction='introduction'
-          name='name'
-          tag={['tag1', 'tag2']}
-          title='title'
-        />
-        <GatheringCard
-          deadline='deadline'
-          introduction='introduction'
-          name='name'
-          tag={['tag1', 'tag2']}
-          title='title'
-        />
-        <GatheringCard
-          deadline='deadline'
-          introduction='introduction'
-          name='name'
-          tag={['tag1', 'tag2']}
-          title='title'
-        />
-        <GatheringCard
-          deadline='deadline'
-          introduction='introduction'
-          name='name'
-          tag={['tag1', 'tag2']}
-          title='title'
-        />
-        <GatheringCard
-          deadline='deadline'
-          introduction='introduction'
-          name='name'
-          tag={['tag1', 'tag2']}
-          title='title'
-        />
-        <GatheringCard
-          deadline='deadline'
-          introduction='introduction'
-          name='name'
-          tag={['tag1', 'tag2']}
-          title='title'
-        />
-        <GatheringCard
-          deadline='deadline'
-          introduction='introduction'
-          name='name'
-          tag={['tag1', 'tag2']}
-          title='title'
-        />
-        <GatheringCard
-          deadline='deadline'
-          introduction='introduction'
-          name='name'
-          tag={['tag1', 'tag2']}
-          title='title'
-        /><GatheringCard
-        deadline='deadline'
-        introduction='introduction'
-        name='name'
-        tag={['tag1', 'tag2']}
-        title='title'
-      />
-      <GatheringCard
-        deadline='deadline'
-        introduction='introduction'
-        name='name'
-        tag={['tag1', 'tag2']}
-        title='title'
-      />
-      <GatheringCard
-        deadline='deadline'
-        introduction='introduction'
-        name='name'
-        tag={['tag1', 'tag2']}
-        title='title'
-      /><GatheringCard
-      deadline='deadline'
-      introduction='introduction'
-      name='name'
-      tag={['tag1', 'tag2']}
-      title='title'
-    />
-    <GatheringCard
-      deadline='deadline'
-      introduction='introduction'
-      name='name'
-      tag={['tag1', 'tag2']}
-      title='title'
-    />
-    <GatheringCard
-      deadline='deadline'
-      introduction='introduction'
-      name='name'
-      tag={['tag1', 'tag2']}
-      title='title'
-    />
+        {items.map(gathering => (
+          <GatheringCard
+            deadline={gathering.deadLine}
+            gatheringId={gathering.gatheringId}
+            introduction={`${gathering.sort} · ${gathering.subject}`}
+            key={gathering.gatheringId}
+            name={gathering.username}
+            tag={gathering.tags}
+            title={gathering.title}
+          />
+        ))}
       </ul>
     </article>
   );
