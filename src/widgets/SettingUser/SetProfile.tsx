d2a0d4291240c49f149b6a6c60a1cf7ccf65307d
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-12-01 20:58:30 +0900

    feat: MyPage 프로필 수정 버튼 추가

--- src/widgets/SettingUser/SetProfile.tsx
+++ src/widgets/SettingUser/SetProfile.tsx
@@ -1,12 +1,20 @@
+import styles from './SetProfile.module.scss';
+
 import { ProfileForm, profileFormConfig } from '@/features/auth';
+import { Button } from '@/shared/ui';
 
 export const SetProfile = () => {
   return (
-    <ProfileForm
-      formConfig={profileFormConfig}
-      onSubmit={data => {
-        console.log(data);
-      }}
-    />
+    <div className={styles.editProfileWrapper}>
+      <ProfileForm
+        formConfig={profileFormConfig}
+        onSubmit={data => {
+          console.log(data);
+        }}
+      />
+      <Button form='profile-form' type='submit'>
+        완료
+      </Button>
+    </div>
   );
 };
