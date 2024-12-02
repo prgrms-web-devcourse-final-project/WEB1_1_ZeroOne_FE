5f49bc42113462e320a8b77281bb903b284819ae
Jo Cheonsan <iii1003@naver.com>
2024-12-02 14:42:48 +0900

    [Feat] 컨택트 모달 및 포폴 컨택트 버튼 구현 (#79)
    * feat: 채팅 모달 헤더 컴포넌트 구현
    
    * feat: 채팅 하단 nav 컴포넌트 구현
    
    * feat: 채팅 입력 컴포넌트 구현
    
    * feat: 채팅방 리스트 아이템 컴포넌트 구현
    
    * feat: 채팅 모달 위젯 컴포넌트 레이아웃 수정
    
    * feat: 채팅 관련 인덱스 정리
    
    * feat: 채팅 방 리스트 타입 정의 및 더미 데이터 연동
    
    * rename: 채팅 모달 ui 폴더 내부로 변경
    
    * feat: contact modal
    
    * style: z-index 부여
    
    * feat: contact modal index 정리
    
    * style: 파비콘 하얀색
    
    * style: global.scss에서는 스타일린트 적용하지 않게 수정
    
    * feat: 포트폴리오 카드의 contact 버튼 컴포넌트화
    
    * feat: 모달 유저네임 받을 수 있게 수정
    
    * feat: 포트폴리오 컨택 버튼 유저네임 전달하게 설정
    
    * feat: ContactModal 구현
    
    * refactor: 프로필 카드 컨택 버튼 스타일 변경
    
    * feat: 컨택 위젯 인덱스 정리

---
+++ src/widgets/ContactModal/ui/ContactModal.tsx
@@ -0,0 +1,55 @@
+import styles from './ContactModal.module.scss';
+
+import Logo from '@/shared/assets/paletteLogo.svg?react';
+import { Modal } from '@/shared/ui';
+
+interface ContactModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  username?: string;
+}
+
+interface ChatOption {
+  id: number;
+  content: string;
+}
+
+export const ContactModal = ({ isOpen, onClose, username }: ContactModalProps) => {
+  const chatOptions: ChatOption[] = [
+    { id: 1, content: '포트폴리오 첨삭 받고 싶어요' },
+    { id: 2, content: '피드백 드리고 싶어요' },
+    { id: 3, content: '프로젝트 같이 하고 싶어요' },
+    { id: 4, content: '커피챗' },
+  ];
+
+  const handleChatOptionClick = (content: string) => {
+    console.log(`Selected option: ${content}`);
+    // 채팅 시작 로직 구현
+  };
+
+  return (
+    <Modal classNames={styles.modalDialogLayout} isOpen={isOpen} onClose={onClose}>
+      <div className={styles.container}>
+        <div className={styles.profileSection}>
+          <div className={styles.logoWrapper}>
+            <Logo className={styles.logo} />
+          </div>
+          <h2 className={styles.profileName}>{username} 님에게 연락하기</h2>
+        </div>
+        <div className={styles.messageContainer}>
+          {chatOptions.map(option => (
+            <button
+              className={styles.messageButton}
+              key={option.id}
+              onClick={() => {
+                handleChatOptionClick(option.content);
+              }}
+            >
+              {option.content}
+            </button>
+          ))}
+        </div>
+      </div>
+    </Modal>
+  );
+};
