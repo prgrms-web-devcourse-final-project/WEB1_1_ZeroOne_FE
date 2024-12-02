c0a24b41bc43aa94bf53620c8a89ac10c8857510
he2e2 <dddd6368@naver.com>
2024-12-02 16:45:01 +0900

    feat: Gathering Detail

--- src/pages/GatheringListPage/GatheringListPage.tsx
+++ src/pages/GatheringListPage/GatheringListPage.tsx
@@ -2,35 +2,30 @@
 import styles from './GatheringListPage.module.scss';
 
 import { useGatheringList } from '@/features/gathering/api/gathering.hook';
-import type { GatheringItemDto } from '@/features/gathering/model/gathering.dto';
+// import type { GatheringItemDto } from '@/features/gathering/model/gathering.dto';
 import { SidebarFilter, PROJECT_CATEGORIES } from '@/shared/ui';
 import { GatheringSelectCon, GatheringGrid } from '@/widgets';
 
-const dummyGatherings: GatheringItemDto[] = Array.from({ length: 9 }, (_, i) => ({
-  gatheringId: i.toString(),
-  userId: i.toString(),
-  contactType: '온라인',
-  sort: '스터디',
-  subject: '개발',
-  period: '1개월',
-  personnel: '1',
-  position: ['개발자'],
-  title: `Sample Gathering`,
-  deadLine: '2022-12-31',
-  username: '홍길동',
-  tags: ['tag1', 'tag2'],
-}));
+// const dummyGatherings: GatheringItemDto[] = Array.from({ length: 9 }, (_, i) => ({
+//   gatheringId: i.toString(),
+//   userId: i.toString(),
+//   contactType: '온라인',
+//   sort: '스터디',
+//   subject: '개발',
+//   period: '1개월',
+//   personnel: '1',
+//   position: ['개발자'],
+//   title: `Sample Gathering`,
+//   deadLine: '2022-12-31',
+//   username: '홍길동',
+//   tags: ['tag1', 'tag2'],
+// }));
 
 export const GatheringListPage = () => {
-  const {
-    items,
-    //  isLoading, isError,
-    ref,
-    isFetchingNextPage,
-  } = useGatheringList('모집중');
+  const { items, isLoading, isError, ref, isFetchingNextPage } = useGatheringList('모집중');
 
-  // if (isLoading) return <div>Loading...</div>;
-  // if (isError) return <div>Error loading gatherings</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error loading gatherings</div>;
 
   return (
     <div className={styles.pageWrapper}>
@@ -43,9 +38,9 @@ export const GatheringListPage = () => {
         </div>
         <div className={styles.mainContent}>
           <GatheringSelectCon />
-          <GatheringGrid items={dummyGatherings} />
+          <GatheringGrid items={items} />
           <div className={styles.loading} ref={ref}>
-            {/* {isFetchingNextPage && <div>Loading more...</div>} */}
+            {isFetchingNextPage && <div>Loading more...</div>}
           </div>
         </div>
       </div>
