d9aa6f58811ff37fb473badec45ddc424ba39932
joarthvr <ioi456859@gmail.com>
2024-12-02 16:21:19 +0900

    feat: 포트폴리오 카드 이미지 렌더링 조건 수정

--- src/features/portfolio/ui/PortfolioCard.tsx
+++ src/features/portfolio/ui/PortfolioCard.tsx
@@ -1,9 +1,12 @@
-import { Link } from 'react-router-dom';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import cn from 'classnames';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { ContactBtn } from './ContactBtn';
 import styles from './PortfolioCard.module.scss';
 
-import profileImg from '@/shared/assets/paletteLogo.svg';
+import DefaultProfilImg from '@/shared/assets/paletteLogo.svg?react';
 
 interface PortfolioCardProps {
   portFolioId: number;
@@ -12,7 +15,7 @@ interface PortfolioCardProps {
   introduction: string;
   majorJobGroup: string;
   minorJobGroup: string;
-  memberImageUrl: string;
+  memberImageUrl?: string;
 }
 
 export const PortfolioCard = ({
@@ -24,29 +27,36 @@ export const PortfolioCard = ({
   minorJobGroup,
   memberImageUrl,
 }: PortfolioCardProps) => {
+  const navigate = useNavigate();
   return (
     <div className={styles.container}>
       <div className={styles.card}>
         <div className={styles.cardHeader}>
           <Link className={styles.cardImg} to={portFolioUrl}>
-            <img
-              alt={`${username}의 프로필 이미지`}
-              src={memberImageUrl || profileImg} // 이미지가 없을 경우 기본 이미지 사용
-            />
+            {memberImageUrl && memberImageUrl.length > 0 ? (
+              <img alt={`${username}의 프로필 이미지`} src={memberImageUrl} />
+            ) : (
+              <DefaultProfilImg height='100%' width='100%' />
+            )}
           </Link>
-          <ContactBtn userName = {username}/>
+          <ContactBtn userName={username} />
         </div>
         <div className={styles.cardFooter}>
           <div className={styles.firstInfo}>
             <span className={styles.name}>{username}</span>
-            <span className={styles.heart}>♥</span>
+            <FontAwesomeIcon
+              className={cn(styles.button, styles.heart)}
+              icon={faHeart}
+              onClick={() => {
+                navigate('/');
+              }}
+            />
           </div>
           <div className={styles.job}>
             <div>
               <span>{minorJobGroup}</span>
               <span>@{majorJobGroup}</span>
             </div>
-            <div>대표 색</div>
           </div>
           <div className={styles.introduction}>{introduction}</div>
         </div>
