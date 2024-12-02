f18e6f3093699674099a372791ed182786304d13
Jo Cheonsan <iii1003@naver.com>
2024-12-02 22:25:35 +0900

    fix: svg 전역 적용으로 인한 페이지 깨짐 현상 수정 (#84)

--- src/features/portfolio/ui/ContactBtn.tsx
+++ src/features/portfolio/ui/ContactBtn.tsx
@@ -9,10 +9,10 @@ interface ContactBtnProps {
 
 export const ContactBtn = ({ userName }: ContactBtnProps) => {
   const open = useModalStore(state => state.actions.open);
-
+  
   const onOpenModal = () => {
     console.log('openContModal');
-    open('contact', userName); // userName을 두 번째 인자로 전달
+    open('contact', userName);  // userName을 두 번째 인자로 전달
   };
 
   return (
