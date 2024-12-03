c6a9b83fc03c8cbc8b4432aa0654e2537cf85369
joarthvr <ioi456859@gmail.com>
2024-12-03 22:20:48 +0900

    feat: 탠스택 쿼리 툴킷 설정

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,17 +1,22 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import { worker } from '../mocks/browser';
-
 import './styles/globals.scss';
 
 if (process.env.NODE_ENV === 'development') {
   void worker.start({ onUnhandledRequest: 'warn' });
 }
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 );
