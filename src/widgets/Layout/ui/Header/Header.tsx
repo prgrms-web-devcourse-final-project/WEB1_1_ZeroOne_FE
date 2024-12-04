cb5d873258eb7ef1524857bf53475d310e07f54f
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-12-04 14:45:43 +0900

    feat: 헤더에 로그아웃 버튼 추가가

--- src/widgets/Layout/ui/Header/Header.tsx
+++ src/widgets/Layout/ui/Header/Header.tsx
@@ -3,21 +3,43 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import cn from 'classnames';
 import React from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useShallow } from 'zustand/shallow';
 
 import styles from './Header.module.scss';
 import { NAV_LINKS } from '../../constants';
 
 //assets
+import { logout } from '@/features/auth/auth.api';
+import { useUserStore } from '@/features/user/model/user.store';
 import Logo from '@/shared/assets/paletteLogo.svg?react';
 //model
 import { useModalStore } from '@/shared/model/modalStore';
 //component
-import { Button } from '@/shared/ui';
+import { Button, customConfirm } from '@/shared/ui';
 
 export const Header = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
   const open = useModalStore(state => state.actions.open);
+  const { userData, actions } = useUserStore(
+    useShallow(state => ({
+      userData: state.userData,
+      actions: state.actions,
+    })),
+  );
+
+  console.log(userData);
+
+  const logoutHandler = async () => {
+    await logout();
+    actions.setUserData(null);
+    await customConfirm({
+      title: '로그아웃',
+      text: '로그아웃 되었습니다.',
+      icon: 'info',
+      showCancelButton: false,
+    });
+  };
 
   return (
     <header className={styles.header}>
@@ -59,13 +81,23 @@ export const Header = () => {
             navigate('/like');
           }}
         />
-        <Button
-          onClick={() => {
-            open('login');
-          }}
-        >
-          로그인
-        </Button>
+        {userData ? (
+          <Button
+            onClick={() => {
+              void logoutHandler();
+            }}
+          >
+            로그아웃
+          </Button>
+        ) : (
+          <Button
+            onClick={() => {
+              open('login');
+            }}
+          >
+            로그인
+          </Button>
+        )}
       </div>
     </header>
   );
