f5524728678e8cfec773a4b1f9305b72780126ad
joarthvr <ioi456859@gmail.com>
2024-12-05 02:39:51 +0900

    style: 작성 버튼 스타일 수정

--- src/pages/WriteGatheringPage/WriteGatheringPage.tsx
+++ src/pages/WriteGatheringPage/WriteGatheringPage.tsx
@@ -1,18 +1,19 @@
+import cn from 'classnames';
 import { FormProvider, useForm } from 'react-hook-form';
 
 import styles from './WriteGatheringPage.module.scss';
 
 import type { CreateGatheringRequest } from '@/features';
 import { useCreateGathering } from '@/features/gathering/lib/hooks';
+import { Button } from '@/shared/ui';
 import { WriteGatheringOpts, WriteGatheringDetail } from '@/widgets';
-
 export const WriteGatheringPage = () => {
   const methods = useForm<CreateGatheringRequest>({
     defaultValues: {
       sort: '',
       subject: '',
       contact: '',
-      personnel: '',
+      personnel: 1,
       period: '',
       position: [],
       title: '',
@@ -28,6 +29,7 @@ export const WriteGatheringPage = () => {
 
   const onSubmit = (data: CreateGatheringRequest) => {
     createGathering(data);
+    console.log(data);
   };
 
   return (
@@ -43,9 +45,11 @@ export const WriteGatheringPage = () => {
             <h2 className={styles.h2}>2. 게시글에 대해서 더 자세히 설명해주세요</h2>
             <WriteGatheringDetail control={methods.control} />
           </section>
-          <button className={styles.submitButton} disabled={isPending} type='submit'>
-            {isPending ? '등록 중...' : '등록하기'}
-          </button>
+          <section className={cn(styles.section, styles.btnCon)}>
+            <Button className={styles.submitButton} disabled={isPending} type='submit'>
+              {isPending ? '등록 중...' : '등록하기'}
+            </Button>
+          </section>
         </form>
       </div>
     </FormProvider>
