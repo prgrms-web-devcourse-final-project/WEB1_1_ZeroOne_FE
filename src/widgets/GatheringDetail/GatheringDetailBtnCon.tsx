e89985626a020e6d8aa9327b47e88683d581c191
joarthvr <ioi456859@gmail.com>
2024-12-05 03:36:58 +0900

    reafactor: 게더링 디테일 페이지 뒤로가기 버튼 추가

--- src/widgets/GatheringDetail/GatheringDetailBtnCon.tsx
+++ src/widgets/GatheringDetail/GatheringDetailBtnCon.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 import styles from './GatheringDetailBtnCon.module.scss';
 
 import { Button } from '@/shared/ui';
@@ -6,9 +8,13 @@ import { Button } from '@/shared/ui';
 export const GatheringDetailBtnCon = () =>
   // {}: GatheringDetailBtnConProps
   {
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+      navigate(-1);
+    };
     return (
       <div className={styles.container}>
-        <Button>뒤로 가기</Button>
+        <Button onClick={handleGoBack}>뒤로 가기</Button>
         <Button>연락하기</Button>
       </div>
     );
