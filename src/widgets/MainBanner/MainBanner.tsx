d93371169402ec68f4b83ebb4ae1e0c184fa030c
he2e2 <dddd6368@naver.com>
2024-12-06 11:12:47 +0900

    feat: auto slider 반영

--- src/widgets/MainBanner/MainBanner.tsx
+++ src/widgets/MainBanner/MainBanner.tsx
@@ -1,14 +1,35 @@
+import Slider from 'react-slick';
+
 import styles from './MainBanner.module.scss';
 
-import Banner from '@/shared/assets/banner.svg?react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import Blue from '@/shared/assets/banner/blue.svg?react';
+import Green from '@/shared/assets/banner/green.svg?react';
+import Purple from '@/shared/assets/banner/purple.svg?react';
+import Red from '@/shared/assets/banner/red.svg?react';
+import Yellow from '@/shared/assets/banner/yellow.svg?react';
 
 export const MainBanner = () => {
+  const settings = {
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    cssEase: 'linear',
+    arrows: false,
+  };
   return (
     <div className={styles.container}>
-      <h2>팔레트에서 자신의 경력과 경험을 소개해주세요</h2>
-      <Banner className={styles.bannerSvg} />
-      <p className={styles.description}>소개 문구</p>
-      <span>PALETTEE</span>
+      <Slider {...settings} className={styles.sliderWrapper}>
+        <Red className={styles.banner} />
+        <Yellow className={styles.banner} />
+        <Blue className={styles.banner} />
+        <Green className={styles.banner} />
+        <Purple className={styles.banner} />
+      </Slider>
     </div>
   );
 };
