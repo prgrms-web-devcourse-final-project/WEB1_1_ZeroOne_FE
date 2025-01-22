63de04da782ba7e97d8daba5c47437736d9bd17a
csk6314 <37896060+csk6314@users.noreply.github.com>
2025-01-22 17:07:34 +0900

    feat: 알림 데이터 타입 변경사항 적용용

--- src/features/notification/notification.dto.ts
+++ src/features/notification/notification.dto.ts
@@ -1,11 +1,16 @@
 import type { NotificationType } from './notification.type';
 
+type ContentType = 'ARCHIVE' | 'GATHERING';
+
 export interface Notification {
   id: number;
   title: string;
   content: string;
   type: NotificationType;
   isRead: boolean;
+  userId: number;
+  contentId: number;
+  likeType: ContentType;
   acceptUrl: string;
   denyUrl: string;
   createdAt: string;
