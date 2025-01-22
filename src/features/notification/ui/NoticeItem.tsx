1e0cb221237070af5285d85dfa5f6fe6680b22dc
csk6314 <37896060+csk6314@users.noreply.github.com>
2025-01-22 17:29:51 +0900

    feat: 좋아요 알람 클릭 시 해당 게시글로 이동

--- src/features/notification/ui/NoticeItem.tsx
+++ src/features/notification/ui/NoticeItem.tsx
@@ -1,6 +1,7 @@
 import { faCircleCheck, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import cn from 'classnames';
+import { useNavigate } from 'react-router-dom';
 
 import styles from './NoticeItem.module.scss';
 import type { Notification } from '../notification.dto';
@@ -14,9 +15,17 @@ export const NoticeItem = ({ notification }: { notification: Notification }) => 
   const { mutate: deleteNotification } = useDeleteNotification(notification.id);
   const { mutate: createChatRoom } = useCreateChatRoom();
   const open = useModalStore(state => state.actions.open);
+  const navigate = useNavigate();
+
+  const handleClickItem = () => {
+    if (notification.type === 'LIKE') {
+      navigate(`/${notification.likeType.toLowerCase()}/${notification.contentId}`);
+      return;
+    }
+  };
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={handleClickItem}>
       <div className={cn(styles.icon, styles[NotificationMap[notification.type].label])}>
         <FontAwesomeIcon icon={NotificationMap[notification.type].icon} />
       </div>
@@ -42,7 +51,8 @@ export const NoticeItem = ({ notification }: { notification: Notification }) => 
         <FontAwesomeIcon
           className={cn(styles.cancel, styles.button)}
           icon={faCircleXmark}
-          onClick={() => {
+          onClick={e => {
+            e.stopPropagation();
             deleteNotification();
           }}
         />
