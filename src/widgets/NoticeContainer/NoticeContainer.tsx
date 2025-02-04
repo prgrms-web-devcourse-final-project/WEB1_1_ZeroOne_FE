d4cdc5e794aa759cb1bd60211a47ec1db3659c56
csk6314 <37896060+csk6314@users.noreply.github.com>
2025-02-04 15:26:33 +0900

    feat: NoticeContainer close transition 종료 시 display none

--- src/widgets/NoticeContainer/NoticeContainer.tsx
+++ src/widgets/NoticeContainer/NoticeContainer.tsx
@@ -1,6 +1,7 @@
 import { faSmile } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import cn from 'classnames';
+import { useEffect, useState } from 'react';
 
 import styles from './NoticeContainer.module.scss';
 
@@ -13,9 +14,34 @@ interface NoticeContainerProps {
 }
 
 export const NoticeContainer = ({ isNotice, notifications }: NoticeContainerProps) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [animationClass, setAnimationClass] = useState('');
+
+  useEffect(() => {
+    if (isNotice) {
+      setIsVisible(true);
+      const transitionHandler = setTimeout(() => {
+        setAnimationClass('slideDown');
+      }, 10);
+
+      return () => {
+        clearTimeout(transitionHandler);
+      };
+    } else {
+      setAnimationClass('slideUp');
+      const transitionHandler = setTimeout(() => {
+        setIsVisible(false);
+      }, 300);
+
+      return () => {
+        clearTimeout(transitionHandler);
+      };
+    }
+  }, [isNotice]);
+
   return (
-    <div className={cn(styles.containerWrapper)}>
-      <div className={cn(styles.container, { [styles.visible]: isNotice })}>
+    <div className={cn(styles.containerWrapper, { [styles.hide]: !isVisible })}>
+      <div className={cn(styles.container, styles[animationClass])}>
         {notifications.length === 0 && (
           <div className={styles.empty}>
             알림이 없습니다 <FontAwesomeIcon icon={faSmile} />
