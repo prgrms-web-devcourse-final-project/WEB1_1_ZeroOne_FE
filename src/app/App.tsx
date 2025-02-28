7e4aaf115d66eaec7115ced2797fd39ec1f267b2
채승규 <37896060+csk6314@users.noreply.github.com>
2025-02-28 21:52:55 +0900

    Dev Merge (#137)
    * [FEAT] 프로필 폼 직무 리스트 및 기타 버그 수정 (#118)
    
    * fix: profile form 직무 카테고리 버그 수정
    
    * rename: feature auth 폴더 정리
    
    * fix: modal 스크롤 방지 시 height 고정 버그 픽스스
    
    * fix: sweetalert 사용 시 body padding 삭제제
    
    * fix: sidebar filter 안움직임 + 가려짐 버그 수정 + sticky 적용용
    
    * [FIX] 포트폴리오 무한 스크롤 이슈 수정 (#124)
    
    * fix: 포트폴리오 무한 스크롤 버그 수정정
    
    * fix: Header 유저 메뉴 UX 개선
    
    * fix: 로그아웃 시 user loading 유지되는 이슈 수정정
    
    * [FEAT] 유저 ROLE 관련 작업 (#122)
    
    * feat: 유저 등록 전 마이페이지 접근 제한 기능 구현
    
    * feat: useRegistAlarm path check 개선
    
    * feat: 유저 수정 페이지에서 portfolioInput required 설정정
    
    * [FEAT] 좋아요 알람 게시글 표시 및 이동 기능 구현 (#128)
    
    * feat: 알림 데이터 타입 변경사항 적용용
    
    * feat: 알림창 켜져있을 때 실시간 알림 목록 업데이트 적용용
    
    * feat: 좋아요 알람 클릭 시 해당 게시글로 이동
    
    * refactor: 헤더 및 알림 리팩토링 및 버그 수정정
    
    * feat: 좋아요 알람에 게시글 제목 표시시
    
    * feat: 알람 게시글 제목 ellipsis 적용
    
    * feat: NoticeContainer close transition 종료 시 display none
    
    * feat: lighthouse-ci-action 버전 업데이트트
    
    * [Feat] 채팅방 생성및 요청 기능 (#127)
    
    * feat: 채팅 생성 기능
    
    * refactor: 게더링 카드 유저 아이디 props 설정
    
    * feat: 채팅 채팅방 목록
    
    * feat: node global 설정
    
    * feat: 채팅방 참여
    
    * feat: 채팅 보내기 기능
    
    * remove: 안쓰는 파일 삭제
    
    * [FEAT] vite 번들 분리 및 highlight.js dynamic import 적용 (#132)
    
    * feat: rollup visualizer 패키지 설치
    
    * feat: manualChunk 번들 분리리
    
    * fix: 기존 highlight.js 적용 안되던 버그 수정
    
    * feat: highlight 언어 dynamic import 적용용
    
    * [REFACTOR]코드 스플리팅  (#133)
    
    * refactor: 라우트 코드 스플리팅
    
    * refactor: 게더링 마크다운프리뷰 레이지로딩 적용
    
    * refactor: 게더링 마크다운 프리뷰 코드스플리팅
    
    * [Fix] 유저페이지 게더링 오류 수정 (#135)
    
    * refactor: 라우트 코드 스플리팅
    
    * refactor: 게더링 마크다운프리뷰 레이지로딩 적용
    
    * refactor: 게더링 마크다운 프리뷰 코드스플리팅
    
    * refactor: 게더링 리스트 전체 조회 수정
    
    * refactor: 게더링 리스트 전체조회 수정
    
    * fix: 유제페이지 게더링 수정
    
    ---------
    
    Co-authored-by: Jo Cheonsan <iii1003@naver.com>

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,19 @@
+import { Suspense } from 'react'; // 코드 스플리팅
 import { RouterProvider } from 'react-router-dom';
 
 import AppRouter from './appRouter';
 import ModalProvider from './ModalProvider';
 import QueryProvider from './QueryProvider';
 
+import { Loader } from '@/shared/ui'; 
+
 function App() {
   return (
     <QueryProvider>
       <ModalProvider>
-        <RouterProvider router={AppRouter()} />
+        <Suspense fallback={<Loader />}>
+          <RouterProvider router={AppRouter()} />
+        </Suspense>
       </ModalProvider>
     </QueryProvider>
   );
