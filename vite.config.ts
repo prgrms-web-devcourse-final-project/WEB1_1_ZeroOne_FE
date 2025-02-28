fc867912b720a2bfdc7a73897c1eed0202b67dae
채승규 <37896060+csk6314@users.noreply.github.com>
2025-02-28 15:44:06 +0900

    [FEAT] vite 번들 분리 및 highlight.js dynamic import 적용 (#132)
    * feat: rollup visualizer 패키지 설치
    
    * feat: manualChunk 번들 분리리
    
    * fix: 기존 highlight.js 적용 안되던 버그 수정
    
    * feat: highlight 언어 dynamic import 적용용

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
 import svgr from 'vite-plugin-svgr';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,7 +11,17 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr(),
+    visualizer({
+      filename: './dist/stats.html',
+      template: 'treemap',
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   css: {
     preprocessorOptions: {
       scss: {
@@ -31,5 +42,51 @@ export default defineConfig({
   },
   define: {
     global: 'window',
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (
+            id.includes('node_modules/@codemirror') ||
+            id.includes('node_modules/@lezer') ||
+            id.includes('node_modules/@uiw')
+          ) {
+            return 'editor';
+          }
+
+          if (
+            id.includes('highlight.js/lib/languages/') ||
+            id.includes('highlight.js/es/languages/')
+          ) {
+            const lang = id.split('/').pop()?.replaceAll('.js', '');
+            return `hljs-${lang}`; // 개별 청크 생성
+          }
+
+          if (id.includes('node_modules/marked') || id.includes('node_modules/highlight.js')) {
+            return 'preview';
+          }
+
+          if (
+            id.includes('node_modules/react-slick') ||
+            id.includes('node_modules/react-select') ||
+            id.includes('node_modules/slick-carousel') ||
+            id.includes('node_modules/react-datepicker') ||
+            id.includes('node_modules/sweetalert2') ||
+            id.includes('node_modules/date-fns')
+          ) {
+            return 'ui';
+          }
+
+          if (id.includes('node_modules/@nivo') || id.includes('node_modules/d3')) {
+            return 'chart';
+          }
+
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
   },
 });
